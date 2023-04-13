665f0bf516cfecc8425de2f1dcc459b019f9d8ca
jskim216 <gerad216@gmail.com>
2023-04-13 13:01:20 +0900

    실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발; section 5 / 패키지 위치 수정

--- src/test/java/jpabook/jpashop/domain/service/MemberServiceTest.java
+++ src/test/java/jpabook/jpashop/domain/service/MemberServiceTest.java
@@ -2,7 +2,8 @@ package jpabook.jpashop.domain.service;
 
 import jakarta.persistence.EntityManager;
 import jpabook.jpashop.domain.Member;
-import jpabook.jpashop.domain.repository.MemberRepository;
+import jpabook.jpashop.repository.MemberRepository;
+import jpabook.jpashop.service.MemberService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,7 +18,8 @@ import static org.junit.Assert.*;
 @Transactional
 public class MemberServiceTest {
 
-    @Autowired MemberService memberService;
+    @Autowired
+    MemberService memberService;
     @Autowired MemberRepository memberRepository;
     @Autowired EntityManager em;
 
